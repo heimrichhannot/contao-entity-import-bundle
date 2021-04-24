d9b91c21fe9adca13fca690f7fbbf37913a573cb
SyZn <1295141+SyZn@users.noreply.github.com>
2021-04-24 15:45:21 +0200

    Added en translation of SSL-Field

--- src/Resources/contao/languages/en/tl_entity_import_source.php
+++ src/Resources/contao/languages/en/tl_entity_import_source.php
@@ -48,6 +48,8 @@ $lang['retrievalType'][\HeimrichHannot\EntityImportBundle\DataContainer\EntityIm
 $lang['type'][\HeimrichHannot\EntityImportBundle\DataContainer\EntityImportSourceContainer::TYPE_DATABASE]                              = 'Database';
 $lang['type'][\HeimrichHannot\EntityImportBundle\DataContainer\EntityImportSourceContainer::TYPE_FILE]                                  = 'File';
 
+$lang['dontCheckSSL'][0]         = 'Don\'t check SSL';
+$lang['dontCheckSSL'][1]         = 'Activate this checkbox if you want to prevent checking of SSL-Certificates for this request. (Do this only if you know what this means!)';
 $lang['sourceUrl'][0]            = 'Url';
 $lang['sourceUrl'][1]            = 'Enter the URL to the file here.';
 $lang['absolutePath'][0]         = 'Absolute file path';
