95e43f1de42f42f451729eb8b3211848feb9666b
Vu Vo <v.vo@heimrich-hannot.de>
2026-01-05 11:14:21 +0100

    autowire

--- src/HeimrichHannotContaoEntityImportBundle.php
+++ src/HeimrichHannotContaoEntityImportBundle.php
@@ -8,14 +8,25 @@
 
 namespace HeimrichHannot\EntityImportBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class HeimrichHannotContaoEntityImportBundle extends Bundle
+class HeimrichHannotContaoEntityImportBundle extends AbstractBundle
 {
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function loadExtension(
+        array $config,
+        ContainerConfigurator $containerConfigurator,
+        ContainerBuilder $containerBuilder,
+    ): void
+    {
+        $containerConfigurator->import('../config/services.yaml');
     }
     protected function getContainerExtensionClass(): string
     {
